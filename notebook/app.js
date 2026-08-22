eea0faba5439814d4f9134f91cfc7cf0c904914c
SarzaminAr <rezahazrati1374.rh@gmail.com>
2026-08-23 00:18:04 +0330

    Add event listeners for target found and lost

--- notebook/app.js
+++ notebook/app.js
@@ -1,1 +1,19 @@
+const video = document.querySelector("#video");
+const target = document.querySelector("[mindar-image-target]");
 
+target.addEventListener("targetFound", () => {
+
+console.log("Target Found");
+
+video.play();
+
+});
+
+
+target.addEventListener("targetLost", () => {
+
+console.log("Target Lost");
+
+video.pause();
+
+});
